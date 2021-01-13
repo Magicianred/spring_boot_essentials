aaad2ecf1dae63b2e24537224603a4609321800e
Raiff Smith Said <raiff.said@gmail.com>
2021-01-13 00:09:28 -0300

    chore: changes on application resources - stacktrace error

--- src/main/java/academy/devdojo/springboot2/service/AnimeService.java
+++ src/main/java/academy/devdojo/springboot2/service/AnimeService.java
@@ -2,7 +2,9 @@ package academy.devdojo.springboot2.service;
 
 import academy.devdojo.springboot2.domain.Anime;
 import academy.devdojo.springboot2.repository.AnimeRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -14,8 +16,11 @@ public class AnimeService {
         return animes;
     }
 
-    public List<Anime> findById() {
-        return List.of(new Anime(1L, "dbz"), new Anime(2L, "cdz"));
+    public Anime findById(long id) {
+        return animes.stream()
+                .filter(anime -> anime.getId().equals(id))
+                .findFirst()
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Anime not found"));
     }
 }
 
